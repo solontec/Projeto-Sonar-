5996727f0ed6d75a16b0c62cf242e0bf59c3c91b
gui <guisolon.tec@gmail.com>
2025-09-02 22:46:29 -0300

    Adicionando Vlibras

--- views/telaLogin.php
+++ views/telaLogin.php
@@ -23,5 +23,17 @@
             <button type="submit">Fazer Login</button>
         </form>
     </div>
+
+    <div vw class="enabled">
+  <div vw-access-button class="active"></div>
+  <div vw-plugin-wrapper>
+    <div class="vw-plugin-top-wrapper"></div>
+  </div>
+</div>
+
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+  new window.VLibras.Widget('https://vlibras.gov.br/app');
+</script>
 </body>
 </html>
