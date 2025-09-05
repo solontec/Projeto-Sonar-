162e7cd88cb2a471a935f85a24bec12ce5f690af
gui <guisolon.tec@gmail.com>
2025-09-05 18:10:56 -0300

    adicionando sql atualizado

--- model/sonar.sql
+++ model/sonar.sql
@@ -1,10 +1,12 @@
 USE sonar;
-SELECT * FROM usuarios;
-create table if not exists usuarios(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(150) NOT NULL,
-email VARCHAR(100) NOT NULL UNIQUE,
-senha VARCHAR(255) NOT NULL,
-tipo_conta ENUM('Cliente', 'Empresa') NOT NULL,
-data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+select * from usuarios;
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    tipo_conta ENUM('Cliente', 'Empresa') NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    token_reset VARCHAR(255),  -- Novo campo para armazenar o token
+    token_expira DATETIME      -- Novo campo para armazenar a data de expiração do token
 );
